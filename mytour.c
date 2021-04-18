e00e39733fb6692228373a99ade3ee01967be0d3
Tanmay Kaushik <60434269+taaanmay@users.noreply.github.com>
2021-04-18 11:52:38 +0100

    Update mytour.c

--- mytour.c
+++ mytour.c
@@ -7,6 +7,8 @@
 #include <sys/time.h>
 #include "sales.h"
 #include <omp.h>
+#include <emmintrin.h>
+#include <xmmintrin.h>
 
 
 float local_sqr(float x)
@@ -18,48 +20,133 @@ float local_dist(const point cities[], int i, int j) {
   return sqrt(local_sqr(cities[i].x-cities[j].x) + local_sqr(cities[i].y-cities[j].y));
 }
 
-void find_tour(const point cities[], int tour[], int ncities)
+void sequential_tour(const point cities[], int tour[], int ncities)
+{
+  int i,j;
+  char *visited = alloca(ncities);
+  int ThisPt, ClosePt=0;
+  float CloseDist;
+  int endtour=0;
+  
+  for (i=0; i<ncities; i++)
+    visited[i]=0;
+  ThisPt = ncities-1;
+  visited[ncities-1] = 1;
+  tour[endtour++] = ncities-1;
+  
+  for (i=1; i<ncities; i++) {
+    CloseDist = DBL_MAX;
+    for (j=0; j<ncities-1; j++) {
+      if (!visited[j]) {
+		floate temp_distance = local_dist(cities, ThisPt, j);
+		if (temp_distance < CloseDist) {
+	  		CloseDist = temp_distance;
+	  		ClosePt = j;
+		}
+      }
+    }
+    tour[endtour++] = ClosePt;
+    visited[ClosePt] = 1;
+    ThisPt = ClosePt;
+  }
+}
+
+// Parallelised and Vectorised Code
+void modified_find_tour(const point cities[], int tour[], int ncities)
 {
   
 	
-	  int j;
-	  char *visited = alloca(ncities);
-	  int ThisPt, ClosePt=0;
-	  float CloseDist;
-	  int endtour=0;
-  
-  	  int i;
-	  #pragma omp parallel for 
-	  for (i=0; i<ncities; i++)
-	  {
-	  	visited[i]=0;
-	  }
-	    
-	  
+  int i,j, index;
+  int ThisPt, ClosePt=0;
+  float CloseDist;
+  int endtour=0;
 
-	  ThisPt = ncities-1;
-	  visited[ncities-1] = 1;
-	  tour[endtour++] = ncities-1;
-	  
-	  for (i=1; i<ncities; i++) {
-	    CloseDist = DBL_MAX;
-	    for (j=0; j<ncities-1; j++) {
-	      if (!visited[j]) {
-		if (local_dist(cities, ThisPt, j) < CloseDist) {
-		  CloseDist = local_dist(cities, ThisPt, j);
-		  ClosePt = j;
-		}
-	      }
+
+  // Visited Array is initialised
+  // Used SIMD and OpenMP for vecotrisation and parallelisation
+  int len_visited = sizeof(float)*ncities + sizeof(float)*(ncities % 4);
+  float visited[len_visited];
+
+  __m128 zero_4 =_mm_set1_ps(0);
+  #pragma omp for 
+  for (i = 0; i < len_visited; i = i+4){
+    _mm_store_ps(&visited[i], zero_4);
+  }
+
+  // Obtain modifiedlength for cases when ncities is not exactly divisible by 4
+  int modified_len = ncities + (4 - (ncities % 4));
+
+  // X and Y coordinate extracted for computing distance
+  float *cityx = malloc(sizeof(float) * modified_len);
+  float *cityy = malloc(sizeof(float) * modified_len);
+  #pragma omp parallel for
+  for (i = 0; i < ncities; i++) {
+      cityx[i] = cities[i].x;
+      cityy[i] = cities[i].y;
+   }
+
+  ThisPt = ncities-1;           
+  visited[ncities-1] = 1;       
+  tour[endtour++] = ncities-1;  
+
+  // Variables initialised for SIMD
+  __m128 this_Pt_X;
+  __m128 this_Pt_Y;
+  __m128 diff_X;
+  __m128 diff_Y;
+  __m128 current_X;
+  __m128 current_Y;
+  __m128 diff_square_X;
+  __m128 diff_square_Y;
+  __m128 distance;
+  
+  
+  for (i=1; i<ncities; i++) {
+    
+  // Calculate Cost
+  float *costArray = malloc(sizeof(float) * modified_len);  
+
+  #pragma omp parallel for private(index, this_Pt_X, this_Pt_Y, current_X, current_Y, diff_X, diff_Y, diff_square_X, diff_square_Y, distance)
+  for (index = 0; index < modified_len; index += 4){
+    this_Pt_X = _mm_set1_ps(cityx[ThisPt]);
+    current_X  = _mm_load_ps(&cityx[index]);
+    
+    this_Pt_Y = _mm_set1_ps(cityy[ThisPt]);
+    current_Y  = _mm_load_ps(&cityy[index]);
+
+    diff_X = _mm_sub_ps(this_Pt_X, current_X);
+    diff_Y = _mm_sub_ps(this_Pt_Y, current_Y);  
+    
+    diff_square_X = _mm_mul_ps(diff_X, diff_X);
+    diff_square_Y = _mm_mul_ps(diff_Y, diff_Y);
+
+    distance = _mm_sqrt_ps(_mm_add_ps(diff_square_X,diff_square_Y));
+    _mm_store_ps(&costArray[index],distance);
+  }
+
+    CloseDist = DBL_MAX;        
+    for (j=0; j<ncities-1; j++) {
+      if (!visited[j] && costArray[j] < CloseDist) {
+	      CloseDist = costArray[j];
+	      ClosePt = j;
 	    }
-	    tour[endtour++] = ClosePt;
-	    visited[ClosePt] = 1;
-	    ThisPt = ClosePt;
-	  }
+    }
+    tour[endtour++] = ClosePt;
+    visited[ClosePt] = 1;
+    ThisPt = ClosePt;
+    free(costArray);
+  }
 	
   
 }
 
 void my_tour(const point cities[], int tour[], int ncities)
 {
-  find_tour(cities, tour, ncities);
+  // Parallelised Code will be used when number of cities is greater than 30,000
+  if(ncities >= 30000){
+  	modified_find_tour(cities, tour, ncities);	
+  }else{
+  	sequential_tour(cities, tour, ncities);	
+  }
+  
 }
